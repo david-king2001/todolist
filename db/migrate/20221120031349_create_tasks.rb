9faae9f236ba3df4955cd5c07f6f210885f1d617
David King <davidking@tamu.edu>
2022-11-20 16:02:50 -0600

    initial commit

---
+++ db/migrate/20221120031349_create_tasks.rb
@@ -0,0 +1,12 @@
+class CreateTasks < ActiveRecord::Migration[7.0]
+  def change
+    create_table :tasks do |t|
+      t.string :name
+      t.string :description
+      t.string :subject
+      t.date :due_date
+
+      t.timestamps
+    end
+  end
+end
